7b1ac328447737277f1b8030fa40554c640304be
David Mendoza <hectormza21@gmail.com>
2022-04-19 13:50:11 -0400

    number project

--- src/projectLogic.js
+++ src/projectLogic.js
@@ -5,7 +5,7 @@ import { sideMenuTabFilters } from '.';
 const projectLiDataBase = [];
 
 //* project 'li' maker.
-const projectFilter = (TDobject) => {
+function projectFilter(TDobject) {
 
     let liAlreadyExists = false; //used to avoid duplcate project tabs.
     let projectSwitch = false; // used to access the project's inmediate todo appender (see below).
@@ -13,46 +13,59 @@ const projectFilter = (TDobject) => {
     //checks if a tab 'li' already exists and stops the code from going further if thats the case.
     if (projectLiDataBase.length >= 1) {
         
-        for (let i = 0; i <= projectLiDataBase.length; i++) {
+        for (let i = 0; i < projectLiDataBase.length; i++) {
 
-            if (projectLiDataBase[i] === TDobject.projectName) {
+            if (projectLiDataBase[i].textContent === TDobject.projectName) {
                 liAlreadyExists = true;
             };
         };
     };
 
+    if (liAlreadyExists === true) {
+        return null;
+    };
+
     /* if the tab li doesnt exists, it gets created and appended to the project ul element
     the text content of this element is the same as the name of the todo object's 'projectName' property (see todoMaker.js). Last, the new tab's text content is added into 'project li database', which will be used the next time the 'projectFilter' function runs in order to prevent duplicates. */
 
-    if (liAlreadyExists === false) {
-
         const projectsUl = document.getElementById('projects-ul');
 
-        const newProjectTab = document.createElement('li');
-            newProjectTab.style.display = "flex";
-    
-        newProjectTab.textContent = `${TDobject.projectName}`;
-    
-        projectsUl.appendChild(newProjectTab);
+        const projectLi = document.createElement('li');
+        projectLi.style.display = 'flex';
 
-        projectLiDataBase.push(newProjectTab.textContent);
+        const projectNameP = document.createElement('p');
+        projectNameP.textContent = TDobject.projectName;
+
+        const numberP = document.createElement('p');
+        /* numberP.textContent = "1"; */
+        
+        projectLi.append(projectNameP, numberP);
+        
+        projectsUl.appendChild(projectLi);
+
+        projectLiDataBase.push(projectNameP);
 
 
     /* This function declares a variable which contains a filtered version of the 'todoObjectDatabase' list. In it will be all the todoObjects whose project names are the same as the new tab 'li' element, then the filter function appends all the todo divs to the 'general div' element, where they are displayed. The reason i put this logic into a function is because i needed to call it in two different places. (see below). */
 
-        const pFilter = () => {
-            const filter = todoObjectDataBase.filter(todoObject => todoObject.projectName === newProjectTab.textContent);
+        function pFilter() {
+            const filter = todoObjectDataBase.filter(todoObject => todoObject.projectName === projectNameP.textContent);
 
             for (let i = 0; i < filter.length; i++) {
                 sideMenuTabFilters.generalDiv.appendChild(filter[i].div);
+
             };
 
-            return {filter};
+            const length = filter.length;
+
+            numberP.textContent = length;
+
+            return length;
         };
 
     /* Here an event listener is added into the newly created li element. This event listener sets the tab switches to false (see index.js), activates the project switch by assigning it to true. And then, after clearing the generalDiv element (the display), the filter function is invoked. */
 
-        newProjectTab.addEventListener('click', () => {
+    projectLi.addEventListener('click', () => {
 
             sideMenuTabFilters.currentSelectedTabCheck.home = false;
             sideMenuTabFilters.currentSelectedTabCheck.today = false;
@@ -63,9 +76,25 @@ const projectFilter = (TDobject) => {
             sideMenuTabFilters.generalDiv.innerHTML = "";
 
             pFilter();
+            /* projectNumberUpdate(pFilter(), numberP); */
+            console.log(pFilter())
         });
 
-    /* here a second event listener is added into the 'addButton' element, this one puts a
+    return {projectSwitch, pFilter};
+};
+
+
+export { projectFilter };
+
+
+const projectNumberUpdate = (filter, numberDisplay) => {
+
+    const tFilterLength = filter;
+    numberDisplay.textContent = tFilterLength === 0? "": tFilterLength;
+
+};
+
+/* here a second event listener is added into the 'addButton' element, this one puts a
     condition in it which only allows the filter function to be accesed if the 'projectSwitch' variable is set to true. This is used in order to append the todo's div as soon as the add todo button is pressed. */
         //todo :this function does not work properly, need to refactor.
         /* todoInputs.addTodoButton.addEventListener('click', () => {
@@ -74,7 +103,7 @@ const projectFilter = (TDobject) => {
                 sideMenuTabFilters.generalDiv.innerHTML = "";
                 pFilter();
             };
-        }); */
+        });
 
         sideMenuTabFilters.homeTab.addEventListener('click', () => {
             projectSwitch = false;
@@ -91,11 +120,4 @@ const projectFilter = (TDobject) => {
         
         //! currently, number updated for projects bugs for an unknown reason.
         //as i progress in T-O-P ill find a way to fix it (hopefully)
-        //todo: number creation and update:
-
-    };
-
-    return {projectSwitch};
-};
-
-export { projectFilter };
+        //todo: number creation and update: */
